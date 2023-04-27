961383aaa4dff082fa18ee915249c373fdd66696
SantiagoOscarVera <santiagovera280@gmail.com>
2023-04-26 23:34:48 -0300

    empiezo en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import {useEffect, useState} from "react"
+import Board from "./components/Board/Board"
 
-function App() {
-  const [count, setCount] = useState(0)
+const emojiList = [..."🍕💣🥬🎩🌮🎱🌶🗡"]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+App = () => {
+const [shuffleMemoBlocks, setShuffleMemoBlocks] = useState([])
+
+useEffect(() => {
+const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList])
+}, [])
+
+const shuffleArray = a => {
+  for (let i = a.length -1; i > 0; i-- ){
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a
+}
+
 }
 
 export default App
