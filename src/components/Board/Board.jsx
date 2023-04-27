e586b9b1b1ab07857f35543b3627ce8cd6a3d8c3
SantiagoOscarVera <santiagovera280@gmail.com>
2023-04-27 00:09:18 -0300

    empiezo en app3

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,12 +1,14 @@
 import Memoblock from "./Memoblock/Memoblock"
 import React from 'react';
 
-const board = ({memoblocks}) => {
+const Board = ({memoBlocks}) => {
     return (
         <main className="board">
-            
+            {memoBlocks.map((memoBlock, i ) => {
+                return <Memoblock key={`${i}_${memoBlock.emoji}`} memoBlock={memoBlock} /> // psamos la key porque lo necesita react para renderizar elementos integrados, el index del map con el emoji nos da un identificador unico para cada bloque y le pasamos el memoblock con toda la informacion del bloque 
+            })}
         </main>
     )
 };
 
-{memoblocks}export default bomain className="board";
+export default Board;
