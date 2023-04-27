c64224fe4b8630338fe24d8c33bb034e175e174a
SantiagoOscarVera <santiagovera280@gmail.com>
2023-04-26 23:54:32 -0300

    empiezo en app1

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,18 @@ App = () => {
 const [shuffleMemoBlocks, setShuffleMemoBlocks] = useState([])
 
 useEffect(() => {
-const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList])
-}, [])
+const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]) // guardamos el array mezclado y se lo pasamos dos veces para que los emojis esten dos veces para que puedan encontrar las parejas
+setShuffleMemoBlocks(shuffledEmojiList.map( (emoji, i ) => ({index:i, emoji, flipped:false}) )) // flipped indica que la carta empueza dada vuelta 
+}, []) // el array de dependencias lo dejamos vacio para que se entienda que se ejecute una sola vez que el componente se renderice
 
-const shuffleArray = a => {
+const shuffleArray = a => { /// devuelve el array pero en posiciones aleatorias 
   for (let i = a.length -1; i > 0; i-- ){
     const j = Math.floor(Math.random() * (i + 1));
     [a[i], a[j]] = [a[j], a[i]];
   }
   return a
 }
+  return <Board memoBlocks={shuffleMemoBlocks} />
 
 }
 
