7c294b963af5b1c61fb110edd526b4f78b76411d
SantiagoOscarVera <santiagovera280@gmail.com>
2023-04-30 17:58:03 -0300

    listo1

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,31 @@ const App = () => {
 const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([])
 const [selectedMemoBlock, setSelectedMemoBlock] = useState(null) // guardar cual es el bloque seleccionado cuando hace click
 const [animating, setAnimating] = useState(false) // para saber si todavia estamos haciendo la animacion para que el usuario no pueda hacer clik en el bloque cuando todavia se esta animando el coso anterios
+const resetGame = () => { // Resetear la aplicacion
+  setTimeLeft(60); // reiniciar el contador de tiempo
+  const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
+  setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i ) => ({index:i, emoji, flipped:false}) ));
+  setSelectedMemoBlock(null);
+  setAnimating(false);
+};
+const [timeLeft, setTimeLeft] = useState(60);
 
 
+const startTimer = () => {
+  const timer = setInterval(() => {
+    setTimeLeft(prevTime => prevTime - 1);
+  }, 1000);
+  return timer;
+};
+
+useEffect(() => {
+  const timer = startTimer();
+  if (timeLeft === 0) {
+    clearInterval(timer);
+    alert("Game Over");
+  }
+  return () => clearInterval(timer);
+}, [timeLeft]);
 
 useEffect(() => {
 const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]) // guardamos el array mezclado y se lo pasamos dos veces para que los emojis esten dos veces para que puedan encontrar las parejas
@@ -26,6 +49,11 @@ const shuffleArray = a => { /// devuelve el array pero en posiciones aleatorias
 }
 
   const handleMemoClick = memoBlock => { 
+
+    if (timeLeft === 0){
+      return 
+    }
+
     const flippedMemoBlock = {...memoBlock, flipped: true}; // seteamos el bloque que fue cliekeado pero el true hace que lo demos vuelta
     let shuffledMemoBlocksCopy = [...shuffledMemoBlocks] // hacemos copia de la lista de bloques
     shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock) // reemplazamos por el bloque seleccionado por el bloque dado vuelta con splice, pasamos el indice del elemento, uno, y el elemento que queremos poner en su lugar
@@ -46,7 +74,11 @@ const shuffleArray = a => { /// devuelve el array pero en posiciones aleatorias
     }
   }
   return (
-    <Board memoBlocks={shuffledMemoBlocks} animating={animating} handleMemoClick={handleMemoClick} /> // es el listado de bloques mezclados
+    <div>
+        <button onClick={resetGame}>Reiniciar</button>
+        <div>Time Left: {timeLeft} seconds</div>
+<Board memoBlocks={shuffledMemoBlocks} animating={animating} handleMemoClick={handleMemoClick} /> {/* // es el listado de bloques mezclados */}
+    </div>
   )
 }
 
