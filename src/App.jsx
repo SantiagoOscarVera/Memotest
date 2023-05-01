5005a3ca4d5f31c3b241ba724b17af844fa83c3f
SantiagoOscarVera <santiagovera280@gmail.com>
2023-04-30 21:27:39 -0300

    conteo empezar

--- src/App.jsx
+++ src/App.jsx
@@ -6,79 +6,92 @@ import Board from "./components/Board/Board.jsx"
 const emojiList = [..."🍕💣🥬🎩🌮🎱🌶🗡😜😪"]
 
 const App = () => {
-const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([])
-const [selectedMemoBlock, setSelectedMemoBlock] = useState(null) // guardar cual es el bloque seleccionado cuando hace click
-const [animating, setAnimating] = useState(false) // para saber si todavia estamos haciendo la animacion para que el usuario no pueda hacer clik en el bloque cuando todavia se esta animando el coso anterios
-const resetGame = () => { // Resetear la aplicacion
-  setTimeLeft(60); // reiniciar el contador de tiempo
-  const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
-  setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i ) => ({index:i, emoji, flipped:false}) ));
-  setSelectedMemoBlock(null);
-  setAnimating(false);
-};
-const [timeLeft, setTimeLeft] = useState(60);
+  const [shuffledMemoBlocks, setShuffledMemoBlocks] = useState([]);
+  const [selectedMemoBlock, setSelectedMemoBlock] = useState(null);
+  const [animating, setAnimating] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(60);
+  const [timerStarted, setTimerStarted] = useState(false); // nuevo estado booleano
 
+  const resetGame = () => {
+    setTimeLeft(60);
+    const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]);
+    setShuffledMemoBlocks(
+      shuffledEmojiList.map((emoji, i) => ({ index: i, emoji, flipped: false }))
+    );
+    setSelectedMemoBlock(null);
+    setAnimating(false);
+    setTimerStarted(false); // reiniciar el estado booleano
+  };
 
-const startTimer = () => {
-  const timer = setInterval(() => {
-    setTimeLeft(prevTime => prevTime - 1);
-  }, 1000);
-  return timer;
-};
+  const startTimer = () => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 1);
+    }, 1000);
+    return timer;
+  };
 
-useEffect(() => {
-  const timer = startTimer();
-  if (timeLeft === 0) {
-    clearInterval(timer);
-    alert("Game Over");
-  }
-  return () => clearInterval(timer);
-}, [timeLeft]);
+  useEffect(() => {
+    let timer;
+    if (timerStarted) { // agregar condición para que el temporizador solo comience si timerStarted es true
+      timer = startTimer();
+    }
+    if (timeLeft === 0) {
+      clearInterval(timer);
+      alert("Game Over");
+    }
+    return () => clearInterval(timer);
+  }, [timeLeft, timerStarted]);
 
-useEffect(() => {
-const shuffledEmojiList = shuffleArray([...emojiList, ...emojiList]) // guardamos el array mezclado y se lo pasamos dos veces para que los emojis esten dos veces para que puedan encontrar las parejas
-setShuffledMemoBlocks(shuffledEmojiList.map( (emoji, i ) => ({index:i, emoji, flipped:false}) )) // flipped indica que la carta empueza dada vuelta 
-}, []) // el array de dependencias lo dejamos vacio para que se entienda que se ejecute una sola vez que el componente se renderice
+  const shuffleArray = (a) => {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  };
 
-const shuffleArray = a => { /// devuelve el array pero en posiciones aleatorias 
-  for (let i = a.length -1; i > 0; i-- ){
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a
-}
+  const handleStartTimerClick = () => {
+    setTimerStarted(true); // establecer timerStarted en true cuando se hace clic en el botón
+  };
 
-  const handleMemoClick = memoBlock => { 
-
-    if (timeLeft === 0){
-      return 
+  const handleMemoClick = (memoBlock) => {
+    if (timeLeft === 0) {
+      return;
     }
 
-    const flippedMemoBlock = {...memoBlock, flipped: true}; // seteamos el bloque que fue cliekeado pero el true hace que lo demos vuelta
-    let shuffledMemoBlocksCopy = [...shuffledMemoBlocks] // hacemos copia de la lista de bloques
-    shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock) // reemplazamos por el bloque seleccionado por el bloque dado vuelta con splice, pasamos el indice del elemento, uno, y el elemento que queremos poner en su lugar
-    setShuffledMemoBlocks(shuffledMemoBlocksCopy) // seteamos el nuevo listado de bloques con el bloque dado vuelta
+    const flippedMemoBlock = { ...memoBlock, flipped: true };
+    let shuffledMemoBlocksCopy = [...shuffledMemoBlocks];
+    shuffledMemoBlocksCopy.splice(memoBlock.index, 1, flippedMemoBlock);
+    setShuffledMemoBlocks(shuffledMemoBlocksCopy);
     if (selectedMemoBlock === null) {
-      setSelectedMemoBlock(memoBlock) // cuando no hay bloque seleccionado es igual al bloque clickeado
-    } else if (selectedMemoBlock.emoji === memoBlock.emoji){ //los bloques/emojis son iguales
-      setSelectedMemoBlock(null) // dejamos todo igual apra que el usuario pueda seguir jugando 
-    } else { // el usuario no acerto
-      setAnimating(true)
+      setSelectedMemoBlock(memoBlock);
+    } else if (selectedMemoBlock.emoji === memoBlock.emoji) {
+      setSelectedMemoBlock(null);
+    } else {
+      setAnimating(true);
       setTimeout(() => {
-        shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock); // reemplazamos los bloques por los mismos bloques dados vuelta
+        shuffledMemoBlocksCopy.splice(memoBlock.index, 1, memoBlock);
         shuffledMemoBlocksCopy.splice(selectedMemoBlock.index, 1, selectedMemoBlock);
-        setShuffledMemoBlocks(shuffledMemoBlocksCopy)
-        setSelectedMemoBlock(null)
-        setAnimating(false) // en false porque ya termino la animacion
-      }, 1000)
+        setShuffledMemoBlocks(shuffledMemoBlocksCopy);
+        setSelectedMemoBlock(null);
+        setAnimating(false);
+      }, 1000);
     }
-  }
+  };
+
   return (
     <div>
-        <button onClick={resetGame}>Reiniciar</button>
-        <div>Time Left: {timeLeft} seconds</div>
-<Board memoBlocks={shuffledMemoBlocks} animating={animating} handleMemoClick={handleMemoClick} /> {/* // es el listado de bloques mezclados */}
+  <button onClick={resetGame}>Reiniciar</button>
+  {timerStarted ? (
+    <div>
+      <button disabled>Empezar el conteo</button>
+      <div>Tiempo restante: {timeLeft} segundos</div>
     </div>
+  ) : (
+    <button onClick={handleStartTimerClick}>Empezar el conteo</button>
+  )}
+  <Board memoBlocks={shuffledMemoBlocks} animating={animating} handleMemoClick={handleMemoClick} /> {/* // es el listado de bloques mezclados */}
+</div>
   )
 }
 
